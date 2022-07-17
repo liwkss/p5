31d05f4d0211fd74ca70e91e861db62b3cdeb6a9
Kit <liwkcuhk+gitlab@gmail.com>
2022-07-17 18:06:25 +0800

    bubble slider

--- bubble/sketch.js
+++ bubble/sketch.js
@@ -1,37 +1,49 @@
-let bubbles=[];
+let bubbles = [];
+let circleNum;
+let circleNumSlider;
 
 function setup() {
   createCanvas(400, 400);
 
-  for(let i=0;i<30;i++){
-    bubbles[i]=new Bubble(random(width),random(height),random(10,40))
-  }
-
+  circleNumSlider = createSlider(10, 100, 50);
+  circleNum = createP();
+  circleNumSlider.changed(recreateBubbles);
+  recreateBubbles();
 }
 
 function draw() {
   background(0);
-  for(let b of bubbles){
+
+  for (let b of bubbles) {
     b.show();
     b.move();
-    let intersect=false;
-    for(let ob of bubbles){
-      if(b !== ob && b.intersects(ob)){
-        intersect=true;
+    b.brightness = 0;
+  }
+
+  for (let i = 0; i < bubbles.length; i++) {
+    for (let j = i + 1; j < bubbles.length; j++) {
+      if (bubbles[i].intersects(bubbles[j])) {
+        bubbles[i].brightness = 255;
+        bubbles[j].brightness = 255;
       }
-    }
-    if(intersect){
-      b.brightness=255;
-    }else{
-      b.brightness=10;
     }
   }
 }
 
-function mousePressed(){
-  for (let i=bubbles.length-1;i>=0;i--){
-    if(bubbles[i].contains(mouseX, mouseY)){
-      bubbles.splice(i,1);
+function recreateBubbles() {
+  circleNum.html(`Number of circles: ${circleNumSlider.value()}`);
+  circleNumSlider.parent(circleNum);
+
+  bubbles = [];
+  for (let i = 0; i < circleNumSlider.value(); i++) {
+    bubbles[i] = new Bubble(random(width), random(height), random(10, 40));
+  }
+}
+
+function mousePressed() {
+  for (let i = bubbles.length - 1; i >= 0; i--) {
+    if (bubbles[i].contains(mouseX, mouseY)) {
+      bubbles.splice(i, 1);
     }
   }
 }
@@ -41,7 +53,7 @@ class Bubble {
     this.x = x;
     this.y = y;
     this.r = r;
-    this.brightness=10;
+    this.brightness = 0;
   }
   show() {
     stroke(255);
@@ -53,18 +65,18 @@ class Bubble {
     this.x += random(-5, 5);
     this.y += random(-5, 5);
   }
-  contains(x,y){
-    //console.log("s"+x+" "+y+" "+this.x+" "+this.y+" "+dist(x,y,this.x,this.y)+" "+this.r)
-    if(dist(x,y,this.x,this.y)<this.r){
+  contains(x, y) {
+    //console.log(`s ${x} ${y} ${this.x} ${this.y} ${dist(x,y,this.x,this.y)} ${this.r}`)
+    if (dist(x, y, this.x, this.y) < this.r) {
       return true;
-    }else{
+    } else {
       return false;
     }
   }
-  intersects(b){
-    if(dist(this.x,this.y,b.x,b.y)<this.r+b.r){
+  intersects(b) {
+    if (dist(this.x, this.y, b.x, b.y) < this.r + b.r) {
       return true;
-    }else{
+    } else {
       return false;
     }
   }
--- up.bat
+++ up.bat
@@ -1,3 +1,3 @@
 git add *
-git commit -m "bubble"
+git commit -m "bubble slider"
 git push  https://github.com/liwkss/p5.git
