5f12c87b3172bae73dd7df780bc2696d9350533b
liwkss <34839664+liwkss@users.noreply.github.com>
2023-07-14 10:40:39 +0800

    Add files via upload

--- csv/sketch.js
+++ csv/sketch.js
@@ -6,16 +6,16 @@ function preload() {
 
 function setup() {
   noCanvas();
-  //loadCSV();	enable this in PRD
-}
-
-function loadCSV(){
+  
   print(table.getRowCount() + ' total rows in table');
   print(table.getColumnCount() + ' total columns in table');
   let r = 0;
+  select('#doseTotal').html(table.getString(r++, 0))
   select('#firstDoseTotal').html(table.getString(r++, 0))
   select('#secondDoseTotal').html(table.getString(r++, 0))
   select('#thirdDoseTotal').html(table.getString(r++, 0))
   select('#fourthDoseTotal').html(table.getString(r++, 0))
   select('#fifthDoseTotal').html(table.getString(r++, 0))
+  select('#sixthDoseTotal').html(table.getString(r++, 0))
+  select('#footnote').html(table.getString(r++, 1))
 }
